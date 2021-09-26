63e56fc74dfd384d16dbc088f7a8d34b1f2dcf64
Sadek Mortada <s.m.mo_2010@hotmail.com>
2021-09-26 21:38:19 +0300

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,28 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-
+    private static Scanner scanner=new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("\nInput Folder/File path. (ex: D:/thumbnails.exe)\n");
-        String path=new Scanner(System.in).next();
+        System.out.println("\nEnter Folder path. (ex: C:/newFolder/thumbnails)\n");
+        String path=scanner.next();
         try {
             File directory = new File(path);
+            System.out.println("\nEnter the type you want to change. (ex: exe, png, etc..)\n");
+            String oldType=scanner.next();
+            System.out.println("\nEnter the new type.");
+            String newType=scanner.next();
             File[] directoryContents = directory.listFiles();
             for (File file : directoryContents)
                 if(file.isFile()){
                     String fileName=file.getName();
                     int index = fileName.lastIndexOf('.');
-                    if(fileName.substring(index).equals(".cache"))
-                        file.renameTo(new File(file.getName().substring(0, index) + ".jpg"));
+                    if(fileName.substring(index).equals("."+oldType))
+                        file.renameTo(new File(file.getName().substring(0, index) + "."+newType));
                 }
+            System.out.println("\nChanges are made, check the application directory for the renamed files.");
         }
         catch(Exception e){
-            System.out.println("\nDirectory doesn't exist..");
+            System.out.println("\nExecution failed!");
         }
     }
 }
